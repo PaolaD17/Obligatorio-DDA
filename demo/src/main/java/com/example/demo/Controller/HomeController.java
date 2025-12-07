a38c0af9adb4cb14dd48539a562ae1c74470db20
Paola Díaz <paolamdiazp@gmail.com>
2025-12-07 18:03:34 -0300

    cambios

--- demo/src/main/java/com/example/demo/Controller/HomeController.java
+++ demo/src/main/java/com/example/demo/Controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.Entity.ContenidoEntity;
 import com.example.demo.Service.UsuarioService;
 
 import org.springframework.ui.Model;
